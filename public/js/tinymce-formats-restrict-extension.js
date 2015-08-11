f6e10091205f21f863f8f328008614d690c5e2d4
23yesil <yigityesilpinar@gmail.com>
2015-08-11 15:07:51 +0200

    To restrict tinyMCE formats
    Added js files for each format

---
+++ public/js/tinymce-formats-restrict-extension.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function ($) {
+    var http = location.protocol;
+    var slashes = http.concat("//");
+    var host = slashes.concat(window.location.hostname);
+    var url = host.concat('/wp-login.php');
+    $("div.entry-content").find("div#extension").html('<p style="color:blue;">You have to be an active user to see this.</p><a style="font-size:1.5em; " href="' + url + '" title="Login">Login</a>');
+});
