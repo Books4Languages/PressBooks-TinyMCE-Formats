e1f74dd56d687c51bcc0a260cc3a1fef293a1024
Antonio Devís <colomett@gmail.com>
2020-01-09 16:08:27 +0100

    tinymce h1 and h2 and pre deactivated

--- tinymce-formats-for-pressbooks.php
+++ tinymce-formats-for-pressbooks.php
@@ -25,7 +25,21 @@
  * Text Domain:       tinymce-formats-for-pressbooks
  */
 
+ /**
+  *  Remove the h1 (1=h1), h2 (2=h2) and Preformatted (Preformatted=pre) tag from the WordPress editor.
+  *
+  *  @since 2.0
+  *  @param   array  $settings  The array of editor settings
+  *  @return  array             The modified edit settings
+  */
 
+ function my_format_TinyMCE( $in ) {
+         $in['block_formats'] = "Paragraph=p; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6";
+     return $in;
+ }
+ add_filter( 'tiny_mce_before_init', 'my_format_TinyMCE' );
+
+ 
 /**
 * Hooks your function into the correct filters
 *
