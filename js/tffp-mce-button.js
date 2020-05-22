0605a5ffc3636e832d02ba10c354c7b825ae0d55
Colomet <colomet@hotmail.com>
2020-05-22 09:32:52 +0200

    New box - New default data

--- js/tffp-mce-button.js
+++ js/tffp-mce-button.js
@@ -11,49 +11,55 @@
                        {
                         text: 'Introduction',
                         onclick: function() {
-                          editor.insertContent("<div class='textbox textbox--introduction'><header class='textbox__header'><h2 class='textbox__title'>Introduction</h2></header><div class='textbox__content'>Type your introduction here.<ul><li>First</li><li>Second</li></ul></div></div>");
+                          editor.insertContent("<div class='textbox textbox--introduction'><header class='textbox__header'><h2 class='textbox__title'>Introduction</h2></header><div class='textbox__content'>[INTRODUCTION]</div></div>");
                           }
                         },
                         {
                         text: 'Form',
                         onclick: function() {
-                        editor.insertContent("<div class='textbox textbox--form'><header class='textbox__header'><h2 class='textbox__title'>Form</h2></header><div class='textbox__content'>Type your form here.<ul><li>First</li><li>Second</li></ul></div></div>");
+                        editor.insertContent("<div class='textbox textbox--form'><header class='textbox__header'><h2 class='textbox__title'>Form</h2></header><div class='textbox__content'>[FORM]</div></div>");
                           }
                         },
                         {
                         text: 'Example',
                         onclick: function() {
-                        editor.insertContent("<div class='textbox textbox--example'><header class='textbox__header'><h2 class='textbox__title'>Example</h2></header><div class='textbox__content'>Type your example here.<ul><li>First</li><li>Second</li></ul></div></div>");
+                        editor.insertContent("<div class='textbox textbox--example'><header class='textbox__header'><h2 class='textbox__title'>Example</h2></header><div class='textbox__content'>[EXAMPLE]</div></div>");
                           }
                         },
                         {
                         text: 'Use',
                         onclick: function() {
-                        editor.insertContent("<div class='textbox textbox--use'><header class='textbox__header'><h2 class='textbox__title'>Use</h2></header><div class='textbox__content'>Type your use here.<ul><li>First</li><li>Second</li></ul></div></div>");
+                        editor.insertContent("<div class='textbox textbox--use'><header class='textbox__header'><h2 class='textbox__title'>Use</h2></header><div class='textbox__content'>[USE]</div></div>");
                           }
                         },
                         {
                         text: 'Summary',
                         onclick: function() {
-                        editor.insertContent("<div class='textbox textbox--summary'><header class='textbox__header'><h2 class='textbox__title'>Summary</h2></header><div class='textbox__content'>Type your summary here.<ul><li>First</li><li>Second</li></ul></div></div>");
+                        editor.insertContent("<div class='textbox textbox--summary'><header class='textbox__header'><h2 class='textbox__title'>Summary</h2></header><div class='textbox__content'>[SUMMARY]</div></div>");
+                          }
+                        },
+                        {
+                        text: 'Related',
+                        onclick: function() {
+                        editor.insertContent("<div class='textbox textbox--related'><header class='textbox__header'><h2 class='textbox__title'>Related</h2></header><div class='textbox__content'>[RELATED]</div></div>");
                           }
                         },
                         {
                         text: 'Exercise',
                         onclick: function() {
-                        editor.insertContent("<div class='textbox textbox--exercise'><header class='textbox__header'><h2 class='textbox__title'>Exercise</h2></header><div class='textbox__content'>Type your exercise here.<ul><li>First</li><li>Second</li></ul></div></div>");
+                        editor.insertContent("<div class='textbox textbox--exercise'><header class='textbox__header'><h2 class='textbox__title'>Exercise</h2></header><div class='textbox__content'>[EXERCISE]</div></div>");
                           }
                         },
                         {
                         text: 'Activity',
                         onclick: function() {
-                        editor.insertContent("<div class='textbox textbox--activity'><header class='textbox__header'><h2 class='textbox__title'>Activity</h2></header><div class='textbox__content'>Type your activity here.<ul><li>First</li><li>Second</li></ul></div></div>");
+                        editor.insertContent("<div class='textbox textbox--activity'><header class='textbox__header'><h2 class='textbox__title'>Activity</h2></header><div class='textbox__content'>[ACTIVITY]</div></div>");
                           }
                         },
                         {
                         text: 'Task',
                         onclick: function() {
-                        editor.insertContent("<div class='textbox textbox--task'><header class='textbox__header'><h2 class='textbox__title'>Task</h2></header><div class='textbox__content'>Type your task here.<ul><li>First</li><li>Second</li></ul></div></div>");
+                        editor.insertContent("<div class='textbox textbox--task'><header class='textbox__header'><h2 class='textbox__title'>Task</h2></header><div class='textbox__content'>[TASK]</div></div>");
                           }
                         }
                        ]
